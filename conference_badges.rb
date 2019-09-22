1540534e7e5fdb88bcb4cab2e59a13adb76ec3da
Anthony N Boyd <anboyd97@gmail.com>
2019-09-22 03:06:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+conference_names=["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
+def badge_maker (name)
+  p "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (name_array)
+  badge_array=[]
+  counter=0
+  name_array.each do |names|
+    badge_array[counter]=badge_maker(names)
+    counter+=1
+  end
+  return badge_array
+end
+
+def assign_rooms (speakers)
+  room_assignments=[]
+  counter=0
+  speakers.each_with_index do |speaker, room|
+    room_assignments[counter]="Hello, #{speaker}! You'll be assigned to room #{room}!"
+    counter+=1
+  end
+  return room_assignments
+end
+
+def printer
+  
+    
